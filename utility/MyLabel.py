4ad4e3fb4d6c3e2d8b462f2cf302f55cf2bc5ec4
MohammadMahdiOmid <mohammadmehdiomid@gmail.com>
2023-01-23 10:30:08 +0330

    Lable file added

---
+++ utility/MyLabel.py
@@ -0,0 +1,44 @@
+from PyQt5.QtWidgets import QApplication, QLabel
+from PyQt5.QtGui import QDrag, QPixmap, QPainter
+from PyQt5.QtCore import QMimeData, Qt
+
+
+class DraggableDroppableLabel(QLabel):
+
+    def __init__(self, widget_type):
+        super().__init__(widget_type)
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.drag_start_position = event.pos()
+            self.setAcceptDrops(True)
+
+    def mouseMoveEvent(self, event):
+        if not (event.buttons() & Qt.LeftButton):
+            print("event.buttons() & Qt.LeftButton + :" + event.buttons() + "   " + Qt.LeftButton)
+            return
+        if (event.pos() - self.drag_start_position).manhattanLength() < QApplication.startDragDistance():
+            return
+        drag = QDrag(self)
+        mimedata = QMimeData()
+        mimedata.setText(self.text())
+        drag.setMimeData(mimedata)
+        pixmap = QPixmap(self.size())
+        painter = QPainter(pixmap)
+        painter.drawPixmap(self.rect(), self.grab())
+        painter.end()
+        drag.setPixmap(pixmap)
+        drag.setHotSpot(event.pos())
+        drag.exec_(Qt.MoveAction)
+
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasText():
+            event.acceptProposedAction()
+
+    def dropEvent(self, event):
+        old_text = self.text()
+        source = event.source()
+        text = event.mimeData().text()
+        self.setText(text)
+        source.setText(old_text)
+        event.acceptProposedAction()
